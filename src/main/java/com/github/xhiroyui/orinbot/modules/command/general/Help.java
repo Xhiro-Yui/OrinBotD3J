b95cc41e6320df2d88fa4c008bad13e5244ea4ed
Xhiro Yui <xhiro.yui@gmail.com>
2019-04-23 12:28:50 +0800

    Reverted a mistakenly changed command

--- src/main/java/com/github/xhiroyui/orinbot/modules/command/general/Help.java
+++ src/main/java/com/github/xhiroyui/orinbot/modules/command/general/Help.java
@@ -1,5 +1,6 @@
 package com.github.xhiroyui.orinbot.modules.command.general;
 
+import com.github.xhiroyui.orinbot.util.BotUtil;
 import com.github.xhiroyui.orinbot.util.CommandUtil;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
@@ -11,9 +12,21 @@ public class Help extends GeneralCommands {
     public Help() {
         super("Provides information for commands (including this one). ",
                 1,
-                List.of("Input prefix"),
-                List.of("prefix")
+                List.of("Command caller"),
+                List.of("help", "h")
         );
+    }
+
+    @Override
+    public Mono<Void> executeCommand(MessageCreateEvent event, String args) {
+        return Mono.justOrEmpty(event)
+                .flatMap(ignored -> processParameters(args))
+                .onErrorResume(error -> BotUtil.COMMAND_ERROR_HANDLER.handle(this, error, event)
+                        .flatMap(errorMessage -> event.getMessage().getChannel()
+                                .flatMap(channel -> channel.createMessage(spec -> spec.setContent(errorMessage))))
+                        .then(Mono.empty()))
+                .flatMap(processedArgs -> runCommand(event, processedArgs))
+                .then();
     }
 
     @Override
