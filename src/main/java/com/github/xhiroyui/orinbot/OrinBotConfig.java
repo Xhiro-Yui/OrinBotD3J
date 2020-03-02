a2c3685a765ecfcabeb4b9d435d1303f231d7b6a
XhiroYui <thewindx@gmail.com>
2020-03-02 10:40:33 +0800

    Test commit

--- src/main/java/com/github/xhiroyui/orinbot/OrinBotConfig.java
+++ src/main/java/com/github/xhiroyui/orinbot/OrinBotConfig.java
@@ -1,5 +1,5 @@
 package com.github.xhiroyui.orinbot;
 
 public interface OrinBotConfig {
-	// Empty interface just to enable abstract level of argument for Spring scanning in the main Spring class
+	// Empty interface just to enable abstract level of argument for Spring scanning in the main Spring class.
 }
